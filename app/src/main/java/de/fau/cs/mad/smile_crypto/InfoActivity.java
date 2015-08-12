24d65b51116721651e63e8b2ac7c5613f06d3a83
Sebastian Nemetz <CFP@posteo.de>
2015-08-12 13:00:29 +0200

    added license information, new design of infoblock (about)

--- app/src/main/java/de/fau/cs/mad/smile_crypto/InfoActivity.java
+++ app/src/main/java/de/fau/cs/mad/smile_crypto/InfoActivity.java
@@ -1,10 +1,16 @@
 package de.fau.cs.mad.smile_crypto;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoActivity extends ActionBarActivity {
     private Toolbar toolbar;
@@ -20,8 +26,68 @@ public class InfoActivity extends ActionBarActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        getSupportFragmentManager().beginTransaction().
-                replace(R.id.currentFragment, new InfoFragment()).commit();
+        final Button author = (Button) findViewById(R.id.button_author);
+        author.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                LayoutInflater inflater = LayoutInflater.from(InfoActivity.this);
+                View v = inflater.inflate(R.layout.scrollable_textview, null);
+
+                TextView license_text = (TextView) v.findViewById(R.id.textview_scrollable);
+                license_text.setText(R.string.author_text);
+
+                AlertDialog alertDialog = new AlertDialog.Builder(InfoActivity.this).create();
+                alertDialog.setTitle(R.string.author);
+                alertDialog.setView(v);
+
+                alertDialog.setButton(getString(R.string.done), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alertDialog.show();
+            }
+        });
+
+        final Button license  = (Button) findViewById(R.id.button_license);
+        license.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                LayoutInflater inflater = LayoutInflater.from(InfoActivity.this);
+                View v = inflater.inflate(R.layout.scrollable_textview, null);
+
+                TextView license_text = (TextView) v.findViewById(R.id.textview_scrollable);
+                license_text.setText(R.string.apache);
+
+                AlertDialog alertDialog = new AlertDialog.Builder(InfoActivity.this).create(); //Read Update
+                alertDialog.setTitle(R.string.license);
+                alertDialog.setView(v);
+
+                alertDialog.setButton(getString(R.string.done), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alertDialog.show();
+            }
+        });
+
+        final Button otherProjects = (Button) findViewById(R.id.button_other_projects);
+        otherProjects.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                LayoutInflater inflater = LayoutInflater.from(InfoActivity.this);
+                View v = inflater.inflate(R.layout.scrollable_textview, null);
+
+                TextView license_text = (TextView) v.findViewById(R.id.textview_scrollable);
+                license_text.setText(R.string.other_projects_list);
+
+                AlertDialog alertDialog = new AlertDialog.Builder(InfoActivity.this).create();
+                alertDialog.setTitle(R.string.other_projects);
+                alertDialog.setView(v);
+
+                alertDialog.setButton(getString(R.string.done), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                alertDialog.show();
+            }
+        });
     }
 
     @Override
