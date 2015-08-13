80e07a835b5514d830855cbee01edbf45063a1c7
Sebastian Nemetz <CFP@posteo.de>
2015-08-13 16:03:00 +0200

    added methods for encrypting mimeMessages

--- app/src/main/java/de/fau/cs/mad/smile_crypto/EncryptMail.java
+++ app/src/main/java/de/fau/cs/mad/smile_crypto/EncryptMail.java
@@ -3,25 +3,22 @@ package de.fau.cs.mad.smile_crypto;
 
 import android.util.Log;
 
-import org.spongycastle.cms.RecipientId;
-import org.spongycastle.cms.RecipientInformation;
-import org.spongycastle.cms.RecipientInformationStore;
-import org.spongycastle.cms.jcajce.JceKeyTransEnvelopedRecipient;
-import org.spongycastle.cms.jcajce.JceKeyTransRecipientId;
+import org.spongycastle.asn1.x509.GeneralName;
+import org.spongycastle.cms.CMSAlgorithm;
+import org.spongycastle.cms.RecipientInfoGenerator;
+import org.spongycastle.cms.jcajce.JceCMSContentEncryptorBuilder;
+import org.spongycastle.cms.jcajce.JceKeyTransRecipientInfoGenerator;
 import org.spongycastle.mail.smime.SMIMEEnvelopedGenerator;
-import org.spongycastle.mail.smime.SMIMEEnvelopedParser;
-import org.spongycastle.mail.smime.SMIMEUtil;
+import org.spongycastle.operator.OutputEncryptor;
 
-import java.io.FileOutputStream;
 import java.security.KeyStore;
 import java.security.Security;
+import java.security.cert.X509CertSelector;
 import java.security.cert.X509Certificate;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
-import de.fau.cs.mad.javax.activation.DataHandler;
 import javax.mail.Address;
-import javax.mail.Message;
-import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
@@ -31,45 +28,105 @@ public class EncryptMail {
         Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
     }
 
-    public MimeMessage encrypt(String alias, MimeMessage msg) {
+    public MimeBodyPart encryptBodyPart(MimeBodyPart mimePart, X509Certificate certificate) {
+        SMIMEEnvelopedGenerator envelopedGenerator = new SMIMEEnvelopedGenerator();
+        try {
+            RecipientInfoGenerator recipientInfoGen = new JceKeyTransRecipientInfoGenerator(certificate);
+            envelopedGenerator.addRecipientInfoGenerator(recipientInfoGen);
+
+            OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(
+                    CMSAlgorithm.AES256_CBC).build();
+
+            return envelopedGenerator.generate(mimePart, encryptor);
+        } catch (Exception e) {
+            Log.e(SMileCrypto.LOG_TAG, "Exception while encrypting MimeBodyPart: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public MimeMessage encryptMessage(MimeMessage message, X509Certificate certificate) {
+        SMIMEEnvelopedGenerator envelopedGenerator = new SMIMEEnvelopedGenerator();
+        try {
+            RecipientInfoGenerator recipientInfoGen = new JceKeyTransRecipientInfoGenerator(certificate);
+            envelopedGenerator.addRecipientInfoGenerator(recipientInfoGen);
+
+            OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(
+                    CMSAlgorithm.AES256_CBC).build();
+
+            MimeBodyPart encryptedContent = envelopedGenerator.generate(message, encryptor);
+
+            MimeMessage result = new MimeMessage(message);
+            result.setContent(encryptedContent.getContent(), encryptedContent.getContentType());
+            result.saveChanges();
+
+            return result;
+        } catch (Exception e) {
+            Log.e(SMileCrypto.LOG_TAG, "Error in encryptMessage: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public MimeMessage encryptMessage(MimeMessage message) {
+        SMIMEEnvelopedGenerator envelopedGenerator = new SMIMEEnvelopedGenerator();
+
+        try {
+            for (Address recipient : message.getAllRecipients()) {
+                ArrayList<X509Certificate> certificates = getCertificatesByAddress((InternetAddress) recipient);
+                for (X509Certificate cert : certificates) {
+                    RecipientInfoGenerator recipientInfoGen = new JceKeyTransRecipientInfoGenerator(cert);
+                    envelopedGenerator.addRecipientInfoGenerator(recipientInfoGen);
+                }
+            }
+            OutputEncryptor encryptor = new JceCMSContentEncryptorBuilder(
+                    CMSAlgorithm.AES256_CBC).build();
+
+            MimeBodyPart encryptedContent = envelopedGenerator.generate(message, encryptor);
+
+            MimeMessage result = new MimeMessage(message);
+            result.setContent(encryptedContent.getContent(), encryptedContent.getContentType());
+            result.saveChanges();
+
+            return result;
+        } catch (Exception e) {
+            Log.e(SMileCrypto.LOG_TAG, "Error in encryptMessage: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public ArrayList<X509Certificate> getCertificatesByAddress(InternetAddress address) {
+        ArrayList<X509Certificate> certificates = new ArrayList<>();
         try {
             KeyStore ks = KeyStore.getInstance("AndroidKeyStore");
             ks.load(null);
-
-            Log.d(SMileCrypto.LOG_TAG, "Alias is: " + alias);
-
-            X509Certificate cert = (X509Certificate) ks.getCertificate(alias);
-            RecipientId recId = new JceKeyTransRecipientId(cert);
-            Log.d(SMileCrypto.LOG_TAG, "Got recId");
-
-            SMIMEEnvelopedGenerator gen = new SMIMEEnvelopedGenerator();
-            gen.addKeyTransRecipient(cert); // TODO: <- Deprecated search alternative
-            MimeBodyPart mbp = new MimeBodyPart();
-            mbp.setDataHandler(msg.getDataHandler());
-            mbp.setHeader("Content-Type", "application/octet-stream");
-            mbp.setHeader("Content-Transfer-Encoding", "binary");
-            MimeBodyPart mp = gen.generate(mbp, SMIMEEnvelopedGenerator.RC2_CBC, "SC");
-
-            //
-            // Get a Session object with the default properties.
-            //
-            Properties props = System.getProperties();
-            Session session = Session.getDefaultInstance(props, null);
-            if (session == null) {
-                Log.d(SMileCrypto.LOG_TAG, "Session is null.");
-                return null;
+            Enumeration e = ks.aliases();
+            while (e.hasMoreElements()) {
+                String alias = (String) e.nextElement();
+                X509Certificate c = (X509Certificate) ks.getCertificate(alias);
+            /* TODO: Use selector?
+                X509CertSelector selector = new X509CertSelector();
+                selector.setMatchAllSubjectAltNames(false);
+                selector.addSubjectAlternativeName(GeneralName.rfc822Name, address.getAddress()); */
+                if(c.getSubjectDN().getName().contains("E="+address.toString()))
+                    certificates.add(c);
             }
-            Log.d(SMileCrypto.LOG_TAG, "Got session.");
-
-            MimeMessage body = new MimeMessage(msg);
-            body.setContent(mp.getContent(), mp.getContentType());
-            return body;
-
-
         } catch (Exception e) {
-            Log.e(SMileCrypto.LOG_TAG, "Error in EncryptMail: " + e.getMessage());
-            e.printStackTrace();
+            Log.e(SMileCrypto.LOG_TAG, "Error in getCertificates:" + e.getMessage());
         }
-        return null;
+        return certificates;
+    }
+
+    public MimeBodyPart encryptAndSign(MimeMessage mimeMessage) {
+        SignMessage signMessage = new SignMessage();
+        MimeMessage encryptedMimeMessage = encryptMessage(mimeMessage);
+
+        try {
+            MimeBodyPart mimeBodyPart = (MimeBodyPart) encryptedMimeMessage.getContent();
+            //TODO: correct cast??
+
+            return signMessage.signEncapsulated(mimeBodyPart, null, null);
+        } catch (Exception e) {
+            Log.e(SMileCrypto.LOG_TAG, "Error in encryptAndSign: " + e.getMessage());
+            return null;
+        }
     }
 }
