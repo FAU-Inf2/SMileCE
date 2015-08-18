1a78d3c89da8f1abde0da7d02776ed68f305e3b9
Benjamin Scheel <b.scheel@posteo.de>
2015-08-18 15:16:00 +0200

    Help screen now uses an expandable list.

--- app/src/main/java/de/fau/cs/mad/smile_crypto/HelpActivity.java
+++ app/src/main/java/de/fau/cs/mad/smile_crypto/HelpActivity.java
@@ -6,121 +6,50 @@ import android.graphics.ColorMatrixColorFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Pair;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 public class HelpActivity extends ActionBarActivity {
     private Toolbar toolbar;
+    ExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<Pair<Integer, String[]>>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.navigation_drawer_help);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final TextView faq_import_cert = (TextView) findViewById(R.id.buttonlike_faq_import_cert);
-        faq_import_cert.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                TextView textView = (TextView) findViewById(R.id.text_faq_import_cert);
-                if (textView.getVisibility() == View.GONE)
-                    textView.setVisibility(View.VISIBLE);
-                else
-                    textView.setVisibility(View.GONE);
-            }
-        });
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
-        final TextView faq_passphrase_saved = (TextView) findViewById(R.id.buttonlike_faq_passphrase_saved);
-        faq_passphrase_saved.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                TextView textView = (TextView) findViewById(R.id.text_faq_passphrase_saved);
-                if (textView.getVisibility() == View.GONE)
-                    textView.setVisibility(View.VISIBLE);
-                else
-                    textView.setVisibility(View.GONE);
-            }
-        });
+        // preparing list data
+        prepareListData();
 
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
-        final TextView faq_meaning_colour_q = (TextView) findViewById(R.id.buttonlike_faq_meaning_colour_q);
-        faq_meaning_colour_q.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                TextView textView = (TextView) findViewById(R.id.text_faq_meaning_colour);
-                RelativeLayout rL0 = (RelativeLayout) findViewById(R.id.valid_00);
-                RelativeLayout rL1 = (RelativeLayout) findViewById(R.id.valid_01);
-                RelativeLayout rL2 = (RelativeLayout) findViewById(R.id.valid_02);
-                RelativeLayout rL3 = (RelativeLayout) findViewById(R.id.valid_03);
-                RelativeLayout rL4 = (RelativeLayout) findViewById(R.id.valid_04);
-                RelativeLayout rL5 = (RelativeLayout) findViewById(R.id.valid_05);
-                RelativeLayout rL6 = (RelativeLayout) findViewById(R.id.valid_06);
-
-                if (textView.getVisibility() == View.GONE) {
-                    textView.setVisibility(View.VISIBLE);
-                    rL0.setVisibility(View.VISIBLE);
-                    rL1.setVisibility(View.VISIBLE);
-                    rL2.setVisibility(View.VISIBLE);
-                    rL3.setVisibility(View.VISIBLE);
-                    rL4.setVisibility(View.VISIBLE);
-                    rL5.setVisibility(View.VISIBLE);
-                    rL6.setVisibility(View.VISIBLE);
-                }
-                else {
-                    textView.setVisibility(View.GONE);
-                    rL0.setVisibility(View.GONE);
-                    rL1.setVisibility(View.GONE);
-                    rL2.setVisibility(View.GONE);
-                    rL3.setVisibility(View.GONE);
-                    rL4.setVisibility(View.GONE);
-                    rL5.setVisibility(View.GONE);
-                    rL6.setVisibility(View.GONE);
-                }
-            }
-        });
-
-
-        final TextView author = (TextView) findViewById(R.id.buttonlike_author);
-        author.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                TextView textView = (TextView) findViewById(R.id.text_author);
-                if (textView.getVisibility() == View.GONE)
-                    textView.setVisibility(View.VISIBLE);
-                else
-                    textView.setVisibility(View.GONE);
-            }
-        });
-
-        final TextView license  = (TextView) findViewById(R.id.buttonlike_license);
-        license.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                TextView textView = (TextView) findViewById(R.id.text_license);
-                if(textView.getVisibility() == View.GONE)
-                    textView.setVisibility(View.VISIBLE);
-                else
-                    textView.setVisibility(View.GONE);
-            }
-        });
-
-        final TextView otherProjects = (TextView) findViewById(R.id.buttonlike_other_projects);
-        otherProjects.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                TextView textView = (TextView) findViewById(R.id.text_projects);
-                if(textView.getVisibility() == View.GONE)
-                    textView.setVisibility(View.VISIBLE);
-                else
-                    textView.setVisibility(View.GONE);
-            }
-        });
-
-        setColours();
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
     }
 
     @Override
@@ -138,126 +67,83 @@ public class HelpActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setColours() {
+    private List<Pair<Integer, String[]>> setColours() {
+        List<Pair<Integer, String[]>> colList = new ArrayList<>();
         // TODO: not implemented
-        RelativeLayout rL0 = (RelativeLayout) findViewById(R.id.valid_00);
-        ImageView validCircle0 = (ImageView) rL0.findViewById(R.id.valid_circle_0);
-        ImageView validCircle1 = (ImageView) rL0.findViewById(R.id.valid_circle_1);
-        ImageView validCircle2 = (ImageView) rL0.findViewById(R.id.valid_circle_2);
-        ImageView validCircle3 = (ImageView) rL0.findViewById(R.id.valid_circle_3);
-        ImageView validCircle4 = (ImageView) rL0.findViewById(R.id.valid_circle_4);
-        TextView text = (TextView) rL0.findViewById(R.id.valid_text);
-        validCircle0.getBackground().setColorFilter(getColorFilter("blue"));
-        validCircle1.getBackground().setColorFilter(getColorFilter("blue"));
-        validCircle2.getBackground().setColorFilter(getColorFilter("blue"));
-        validCircle3.getBackground().setColorFilter(getColorFilter("blue"));
-        validCircle4.getBackground().setColorFilter(getColorFilter("blue"));
-        text.setText(R.string.faq_colour_00);
+        String[] c0 = {"#0000ff", "#0000ff", "#0000ff", "#0000ff", "#0000ff", getString(R.string.faq_colour_00)};
+        colList.add(Pair.create(1, c0));
 
-        //} else if(years.getYears() > 1 || (years.getYears() == 1 && months.getMonths() > 0)) {
-        RelativeLayout rL1 = (RelativeLayout) findViewById(R.id.valid_01);
-        validCircle0 = (ImageView) rL1.findViewById(R.id.valid_circle_0);
-        validCircle1 = (ImageView) rL1.findViewById(R.id.valid_circle_1);
-        validCircle2 = (ImageView) rL1.findViewById(R.id.valid_circle_2);
-        validCircle3 = (ImageView) rL1.findViewById(R.id.valid_circle_3);
-        validCircle4 = (ImageView) rL1.findViewById(R.id.valid_circle_4);
-        text = (TextView) rL1.findViewById(R.id.valid_text);
-        validCircle0.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        validCircle1.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        validCircle2.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        validCircle3.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        validCircle4.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        text.setText(R.string.faq_colour_01);
+        String[] c1 = {"#00ff00", "#00ff00", "#00ff00", "#00ff00", "#00ff00", getString(R.string.faq_colour_01)};
+        colList.add(Pair.create(1, c1));
 
-        //} else if(months.getMonths() > 5) {
-        RelativeLayout rL2 = (RelativeLayout) findViewById(R.id.valid_02);
-        validCircle0 = (ImageView) rL2.findViewById(R.id.valid_circle_0);
-        validCircle1 = (ImageView) rL2.findViewById(R.id.valid_circle_1);
-        validCircle2 = (ImageView) rL2.findViewById(R.id.valid_circle_2);
-        validCircle3 = (ImageView) rL2.findViewById(R.id.valid_circle_3);
-        validCircle4 = (ImageView) rL2.findViewById(R.id.valid_circle_4);
-        text = (TextView) rL2.findViewById(R.id.valid_text);
-        validCircle0.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        validCircle1.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        validCircle2.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        validCircle3.getBackground().setColorFilter(getColorFilter("#00ff00"));
-        validCircle4.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        text.setText(R.string.faq_colour_02);
+        String[] c2 = {"#00ff00", "#00ff00", "#00ff00", "#00ff00", "#d3d3d3", getString(R.string.faq_colour_02)};
+        colList.add(Pair.create(1, c2));
 
-        //else if (months.getMonths() > 2) {
-        RelativeLayout rL3 = (RelativeLayout) findViewById(R.id.valid_03);
-        validCircle0 = (ImageView) rL3.findViewById(R.id.valid_circle_0);
-        validCircle1 = (ImageView) rL3.findViewById(R.id.valid_circle_1);
-        validCircle2 = (ImageView) rL3.findViewById(R.id.valid_circle_2);
-        validCircle3 = (ImageView) rL3.findViewById(R.id.valid_circle_3);
-        validCircle4 = (ImageView) rL3.findViewById(R.id.valid_circle_4);
-        text = (TextView) rL3.findViewById(R.id.valid_text);
-        validCircle0.getBackground().setColorFilter(getColorFilter("#FFA500"));
-        validCircle1.getBackground().setColorFilter(getColorFilter("#FFA500"));
-        validCircle2.getBackground().setColorFilter(getColorFilter("#FFA500"));
-        validCircle3.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle4.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        text.setText(R.string.faq_colour_03);
+        String[] c3 = {"#FFA500", "#FFA500", "#FFA500", "#d3d3d3", "#d3d3d3", getString(R.string.faq_colour_03)};
+        colList.add(Pair.create(1, c3));
 
-        //else if(months.getMonths() > 0) {
-        RelativeLayout rL4 = (RelativeLayout) findViewById(R.id.valid_04);
-        validCircle0 = (ImageView) rL4.findViewById(R.id.valid_circle_0);
-        validCircle1 = (ImageView) rL4.findViewById(R.id.valid_circle_1);
-        validCircle2 = (ImageView) rL4.findViewById(R.id.valid_circle_2);
-        validCircle3 = (ImageView) rL4.findViewById(R.id.valid_circle_3);
-        validCircle4 = (ImageView) rL4.findViewById(R.id.valid_circle_4);
-        text = (TextView) rL4.findViewById(R.id.valid_text);
-        validCircle0.getBackground().setColorFilter(getColorFilter("#FFA500"));
-        validCircle1.getBackground().setColorFilter(getColorFilter("#FFA500"));
-        validCircle2.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle3.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle4.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        text.setText(R.string.faq_colour_04);
+        String[] c4 = {"#FFA500", "#FFA500", "#d3d3d3", "#d3d3d3", "#d3d3d3", getString(R.string.faq_colour_04)};
+        colList.add(Pair.create(1, c4));
 
-        //else if(valid.getMillis() > today.getMillis()) {
-        RelativeLayout rL5 = (RelativeLayout) findViewById(R.id.valid_05);
-        validCircle0 = (ImageView) rL5.findViewById(R.id.valid_circle_0);
-        validCircle1 = (ImageView) rL5.findViewById(R.id.valid_circle_1);
-        validCircle2 = (ImageView) rL5.findViewById(R.id.valid_circle_2);
-        validCircle3 = (ImageView) rL5.findViewById(R.id.valid_circle_3);
-        validCircle4 = (ImageView) rL5.findViewById(R.id.valid_circle_4);
-        text = (TextView) rL5.findViewById(R.id.valid_text);
-        validCircle0.getBackground().setColorFilter(getColorFilter("#FFA500"));
-        validCircle1.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle2.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle3.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle4.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        text.setText(R.string.faq_colour_05);
+        String[] c5 = {"#FFA500", "#d3d3d3", "#d3d3d3", "#d3d3d3", "#d3d3d3", getString(R.string.faq_colour_05)};
+        colList.add(Pair.create(1, c5));
 
-        // if(valid.getMillis() <= today.getMillis())
-        RelativeLayout rL6 = (RelativeLayout) findViewById(R.id.valid_06);
-        validCircle0 = (ImageView) rL6.findViewById(R.id.valid_circle_0);
-        validCircle1 = (ImageView) rL6.findViewById(R.id.valid_circle_1);
-        validCircle2 = (ImageView) rL6.findViewById(R.id.valid_circle_2);
-        validCircle3 = (ImageView) rL6.findViewById(R.id.valid_circle_3);
-        validCircle4 = (ImageView) rL6.findViewById(R.id.valid_circle_4);
-        text = (TextView) rL6.findViewById(R.id.valid_text);
-        validCircle0.getBackground().setColorFilter(getColorFilter("red"));
-        validCircle1.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle2.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle3.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-        validCircle4.getBackground().setColorFilter(getColorFilter("#d3d3d3"));
-                text.setText(R.string.faq_colour_06);
+        String[] c6 = {"#ff0000", "#d3d3d3", "#d3d3d3", "#d3d3d3", "#d3d3d3", getString(R.string.faq_colour_06)};
+        colList.add(Pair.create(1, c6));
 
+        return colList;
     }
 
-    private ColorFilter getColorFilter(String color) {
-        int iColor = Color.parseColor(color);
 
-        int red = (iColor & 0xFF0000) / 0xFFFF;
-        int green = (iColor & 0xFF00) / 0xFF;
-        int blue = iColor & 0xFF;
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<Pair<Integer, String[]>>>();
 
-        float[] matrix = { 0, 0, 0, 0, red
-                , 0, 0, 0, 0, green
-                , 0, 0, 0, 0, blue
-                , 0, 0, 0, 1, 0 };
+        // Adding child data
+        listDataHeader.add(getString(R.string.faq_import_cert_q));
+        listDataHeader.add(getString(R.string.faq_passphrase_saved_q));
+        listDataHeader.add(getString(R.string.faq_meaning_colour_q));
+        listDataHeader.add(getString(R.string.faq_author));
+        listDataHeader.add(getString(R.string.faq_license));
+        listDataHeader.add(getString(R.string.faq_other_projects));
 
-        return new ColorMatrixColorFilter(matrix);
+        String[] certText = {getString(R.string.faq_import_cert)};
+        Pair<Integer, String[]> cert = Pair.create(0, certText);
+        List<Pair<Integer, String[]>> importCert = new ArrayList<>();
+        importCert.add(cert);
+
+        String[] pwText = {getString(R.string.faq_passphrase_saved)};
+        Pair<Integer, String[]> pw = Pair.create(0, pwText);
+        List<Pair<Integer, String[]>> pwList = new ArrayList<>();
+        pwList.add(pw);
+
+        String[] colorText = {getString(R.string.faq_meaning_colour)};
+        Pair<Integer, String[]> col = Pair.create(0, colorText);
+        List<Pair<Integer, String[]>> colList = new ArrayList<>();
+        colList.add(col);
+        colList.addAll(setColours());
+
+        String[] authorText = {getString(R.string.author_text)};
+        Pair<Integer, String[]> author = Pair.create(0, authorText);
+        List<Pair<Integer, String[]>> authorList = new ArrayList<>();
+        authorList.add(author);
+
+        String[] licenceText = {getString(R.string.apache)};
+        Pair<Integer, String[]> licence = Pair.create(0, licenceText);
+        List<Pair<Integer, String[]>> licenceList = new ArrayList<>();
+        licenceList.add(licence);
+
+        String[] othersText = {getString(R.string.other_projects_list)};
+        Pair<Integer, String[]> others = Pair.create(0, othersText);
+        List<Pair<Integer, String[]>> othersList = new ArrayList<>();
+        othersList.add(others);
+
+        listDataChild.put(listDataHeader.get(0), importCert);
+        listDataChild.put(listDataHeader.get(1), pwList);
+        listDataChild.put(listDataHeader.get(2), colList);
+        listDataChild.put(listDataHeader.get(3), authorList);
+        listDataChild.put(listDataHeader.get(4), licenceList);
+        listDataChild.put(listDataHeader.get(5), othersList);
+
     }
 }
