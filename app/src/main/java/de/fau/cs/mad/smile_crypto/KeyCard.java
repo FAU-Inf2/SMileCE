1b4fa4c088d539f87f58f3e5c5dbfc055a984276
Benjamin Scheel <b.scheel@posteo.de>
2015-08-19 20:38:44 +0200

    Replaced QuickContactBadge with round button.

--- app/src/main/java/de/fau/cs/mad/smile_crypto/KeyCard.java
+++ app/src/main/java/de/fau/cs/mad/smile_crypto/KeyCard.java
@@ -3,18 +3,31 @@ package de.fau.cs.mad.smile_crypto;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.QuickContactBadge;
 import android.widget.TextView;
@@ -31,6 +44,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.mail.Address;
 
@@ -40,7 +55,7 @@ import it.gmariotti.cardslib.library.internal.CardHeader;
 public class KeyCard extends Card {
     TextView email;
     TextView valid_until;
-    QuickContactBadge contact;
+    Button contact;
     String name;
     ImageView validCircle0;
     ImageView validCircle1;
@@ -49,6 +64,26 @@ public class KeyCard extends Card {
     ImageView validCircle4;
     KeyInfo keyInfo;
     Context context;
+
+    private static List<Integer> materialColors = Arrays.asList(
+            0xffe57373,
+            0xfff06292,
+            0xffba68c8,
+            0xff9575cd,
+            0xff7986cb,
+            0xff64b5f6,
+            0xff4fc3f7,
+            0xff4dd0e1,
+            0xff4db6ac,
+            0xff81c784,
+            0xffaed581,
+            0xffff8a65,
+            0xffd4e157,
+            0xffffd54f,
+            0xffffb74d,
+            0xffa1887f,
+            0xff90a4ae
+    );
 
 
     public KeyCard(Context context) {
@@ -71,7 +106,7 @@ public class KeyCard extends Card {
     public void setupInnerViewElements(ViewGroup parent, View view) {
         email = (TextView) parent.findViewById(R.id.email);
         valid_until = (TextView) parent.findViewById(R.id.valid_until);
-        contact = (QuickContactBadge) parent.findViewById(R.id.badge);
+        contact = (Button) parent.findViewById(R.id.badge);
         name = "";
         validCircle0 = (ImageView) parent.findViewById(R.id.valid_circle_0);
         validCircle1 = (ImageView) parent.findViewById(R.id.valid_circle_1);
@@ -84,19 +119,88 @@ public class KeyCard extends Card {
         }
     }
 
-    private long getContactIDByEmail(String email) {
+    private Bitmap getCroppedBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output;
+    }
+
+
+    private Bitmap generateCircleBitmap(int circleColor, float diameterDP, String text){
+        /**
+         *
+         * http://stackoverflow.com/questions/31168636/rounded-quickcontactbadge-with-text
+         */
+        final int textColor = 0xffffffff;
+
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float diameterPixels = diameterDP * (metrics.densityDpi / 160f);
+        float radiusPixels = diameterPixels/2;
+
+        // Create the bitmap
+        Bitmap output = Bitmap.createBitmap((int) diameterPixels, (int) diameterPixels, Bitmap.Config.ARGB_8888);
+
+        // Create the canvas to draw on
+        Canvas canvas = new Canvas(output);
+        canvas.drawARGB(0, 0, 0, 0);
+
+        // Draw the circle
+        final Paint paintC = new Paint();
+        paintC.setAntiAlias(true);
+        paintC.setColor(circleColor);
+        canvas.drawCircle(radiusPixels, radiusPixels, radiusPixels, paintC);
+
+        // Draw the text
+        if (text != null && text.length() > 0) {
+            final Paint paintT = new Paint();
+            paintT.setColor(textColor);
+            paintT.setAntiAlias(true);
+            paintT.setTextSize(radiusPixels * 2);
+            paintT.setTypeface(Typeface.SANS_SERIF);
+            final Rect textBounds = new Rect();
+            paintT.getTextBounds(text, 0, text.length(), textBounds);
+            canvas.drawText(text, radiusPixels - textBounds.exactCenterX(), radiusPixels - textBounds.exactCenterY(), paintT);
+        }
+
+        return output;
+    }
+
+    private int getMaterialColor(Object key) {
+        return materialColors.get(Math.abs(key.hashCode()) % materialColors.size());
+    }
+
+    private void setContactBadge(final String email) {
         Uri uri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Email.CONTENT_FILTER_URI, Uri.encode(email));
-        String name = "?";
-        long contactId =0;
+        String name = "";
+        String lookUpKey = "";
+        String thumb = "";
 
         ContentResolver contentResolver = context.getContentResolver();
-        Cursor contactLookup = contentResolver.query(uri, new String[] {ContactsContract.Data.CONTACT_ID, ContactsContract.Data.DISPLAY_NAME }, null, null, null);
+        Cursor contactLookup = contentResolver.query(uri, new String[]{ContactsContract.Data.LOOKUP_KEY, ContactsContract.Data.DISPLAY_NAME, ContactsContract.Data.PHOTO_THUMBNAIL_URI}, null, null, null);
 
         try {
             if (contactLookup != null && contactLookup.getCount() > 0) {
 
                 contactLookup.moveToNext();
-                contactId = contactLookup.getLong(contactLookup.getColumnIndex(ContactsContract.Data.CONTACT_ID));
+                lookUpKey = contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.LOOKUP_KEY));
+                name = contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+                thumb = contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.PHOTO_THUMBNAIL_URI));
 
             }
         } finally {
@@ -104,34 +208,33 @@ public class KeyCard extends Card {
                 contactLookup.close();
             }
         }
+        Log.d(SMileCrypto.LOG_TAG, "mail: " + email);
+        Log.d(SMileCrypto.LOG_TAG, "thumb: " + thumb);
+        Log.d(SMileCrypto.LOG_TAG, "name: " + name);
+        Log.d(SMileCrypto.LOG_TAG, "key: " + lookUpKey);
 
-        return contactId;
-    }
+        final Uri lookupUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookUpKey);
 
-    private void setContactBadge(String email) {
-        long contactID = getContactIDByEmail(email);
-        Log.e(SMileCrypto.LOG_TAG, "ContactID = " + contactID);
-        contact.assignContactFromEmail(email, false);
-        Bitmap photo = null;
-
-        try {
-            Uri contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactID);
-            Log.e(SMileCrypto.LOG_TAG, "ContactUri = " + contactUri);
-            InputStream inputStream = ContactsContract.Contacts.openContactPhotoInputStream(context.getContentResolver(), contactUri, false);
-
-            if (inputStream != null) {
-                Log.e(SMileCrypto.LOG_TAG, "Image found");
-                photo = BitmapFactory.decodeStream(inputStream);
-                contact.setImageBitmap(photo);
-                inputStream.close();
-            } else {
-                Log.e(SMileCrypto.LOG_TAG, "Image not found");
-                contact.setImageToDefault();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(thumb != null && thumb.length() > 0) {
+            InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(context.getContentResolver(), lookupUri, false);
+            Bitmap bmp = getCroppedBitmap(BitmapFactory.decodeStream(input));
+            BitmapDrawable bdrawable = new BitmapDrawable(context.getResources(), bmp);
+            contact.setBackground(bdrawable);
+            Log.d(SMileCrypto.LOG_TAG, "Thumbnail found.");
+        } else {
+            Log.d(SMileCrypto.LOG_TAG, "Thumbnail not found.");
+            String initial = (name.length() > 0) ? name.substring(0, 1) : keyInfo.contact.substring(0, 1);
+            BitmapDrawable bdrawable = new BitmapDrawable(context.getResources(), generateCircleBitmap(getMaterialColor(initial), 42, initial));
+            contact.setBackground(bdrawable);
         }
+
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactsContract.Intents.SHOW_OR_CREATE_CONTACT, Uri.parse("mailto:" + email));
+                context.startActivity(intent);
+            }
+        });
     }
 
     private void setData(KeyInfo keyInfo) {
