bb878ef3c0b75a8b9bf4e1af82a18d9b6f3707ba
Sebastian Nemetz <CFP@posteo.de>
2015-08-19 22:22:12 +0200

    changed values of status codes by grouping them

--- app/src/main/java/de/fau/cs/mad/smile_crypto/SMileCrypto.java
+++ app/src/main/java/de/fau/cs/mad/smile_crypto/SMileCrypto.java
@@ -13,20 +13,30 @@ public class SMileCrypto extends Application {
 
     public static int EXIT_STATUS;
 
+    //success
     public static final int STATUS_SUCCESS = 0;
+
+    //problems with certificates/passphrases
     public static final int STATUS_NO_PASSPHRASE_AVAILABLE = 1;
     public static final int STATUS_NO_CERTIFICATE_FOUND = 2;
     public static final int STATUS_INVALID_CERTIFICATE_STORED = 3; // no p12 was saved, invalid p12 was saved, no private key from p12
-    public static final int STATUS_INVALID_PARAMETER = 4; // e.g. null as param, could not be resolved to smth working
-    public static final int STATUS_NO_VALID_MIMEMESSAGE_IN_FILE = 5;
-    public static final int STATUS_NO_RECIPIENTS_FOUND = 6;
-    public static final int STATUS_WRONG_PASSPHRASE = 7;
+    public static final int STATUS_CERTIFICATE_ALREADY_IMPORTED = 4;
+    public static final int STATUS_WRONG_PASSPHRASE = 5;
 
-    public static final int STATUS_CERTIFICATE_ALREADY_IMPORTED = 8;
+    //problems with MimeMessage (e.g. reading, creating…)
+    public static final int STATUS_NO_VALID_MIMEMESSAGE_IN_FILE = 10;
+    public static final int STATUS_NO_VALID_MIMEMESSAGE = 11;
+    public static final int STATUS_NO_RECIPIENTS_FOUND = 12;
 
-    public static final int STATUS_ERROR_ASYNC_TASK = 10;
-    public static final int STATUS_UNKNOWN_ERROR = 15;
+    //problems with decryption/encryption/signing
+    public static final int STATUS_DECRYPTION_FAILED = 15;
 
+    // other problems
+    public static final int STATUS_INVALID_PARAMETER = 20; // e.g. null as param, could not be resolved to smth working
+    public static final int STATUS_ERROR_ASYNC_TASK = 21;
+
+    //unknown error -- status code is the answer ;-)
+    public static final int STATUS_UNKNOWN_ERROR = 42;
 
     /**
      * If this is enabled there will be additional logging information sent to
