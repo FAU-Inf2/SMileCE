4b807cd040357d1be4bffc2c98808d022f23e223
Benjamin Scheel <b.scheel@posteo.de>
2015-09-10 13:56:01 +0200

    Fixed issue with multiple mail addresses.

--- app/src/main/java/de/fau/cs/mad/smile/android/encryption/ui/activity/items/PersonalAndCAInformationItem.java
+++ app/src/main/java/de/fau/cs/mad/smile/android/encryption/ui/activity/items/PersonalAndCAInformationItem.java
@@ -21,8 +21,8 @@ public class PersonalAndCAInformationItem extends AbstractCertificateInfoItem {
 
     public void buildComplex(LinkedHashMap<String, String[]> data) {
         for(String[] arr : data.values()) {
-            if(arr.length != 2) {
-                throw new IllegalArgumentException("Each value of the hash map must have a size of two.");
+            if(arr.length < 2) {
+                throw new IllegalArgumentException("Each value of the hash map must have a minimal size of two.");
             }
         }
         this.data = data;
@@ -36,13 +36,15 @@ public class PersonalAndCAInformationItem extends AbstractCertificateInfoItem {
         View convertView = layoutInflater.inflate(R.layout.personal_information, parent, false);
         TableLayout table = (TableLayout) convertView.findViewById(R.id.table01);
         for(String key : data.keySet()) {
-            LinearLayout row = (LinearLayout) layoutInflater.inflate(R.layout.table_row, table, false);
-            TextView name = (TextView) row.findViewById(R.id.column_left);
-            TextView entry = (TextView) row.findViewById(R.id.column_right);
             String[] values = data.get(key);
-            name.setText(values[0]);
-            entry.setText(values[1]);
-            table.addView(row);
+            for (int i = 1; i < values.length; ++i) {
+                LinearLayout row = (LinearLayout) layoutInflater.inflate(R.layout.table_row, table, false);
+                TextView name = (TextView) row.findViewById(R.id.column_left);
+                TextView entry = (TextView) row.findViewById(R.id.column_right);
+                name.setText(values[0]);
+                entry.setText(values[i]);
+                table.addView(row);
+            }
         }
         return convertView;
     }
